5a5bc0c30814f6464ca1311b29af29d56d97960a
Mizanur Rahman Rony <rmizanurrahman76@gmail.com>
2020-10-13 09:19:32 +0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,22 @@
-<?p
-
->
+<?php
+    $connect = mysqli_connect("localhost","root","","orphanage management");
+    if($connect-> connect_error){
+        die("Connection_failed:". $connect-> connect_error);
+    }
     
+    $sql ="SELECT designation,age,gender,address,number,name from bot";
+    $result = $connect-> query($sql);
+    
+    if($result-> num_rows >0)
+    {
+        while($row=$result-> fetch_assoc()){
+            echo "<tr><td>".$row["designation"]."</td><td>".$row["age"]."</td><td>".$row["gender"] ."</td><td>".$row["address"]."</td><td>".$row["number"] ."</td><td>".$row["name"]."</td></tr>";
+            
+        }
+        echo "</table>";
+    }
+    else{
+        echo"no result";
+    }
+    $connect-> close();
+    ?>
